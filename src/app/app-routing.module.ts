a3a46e4bcd6cb7e58185978d61f8ca280299b8c9
Igor Meireles de Araújo <igoraraujo@ufpa.br>
2018-07-31 20:13:27 -0300

    Added Document Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import { HomeComponent } from './components/home/home.component';
+import { DocumentComponent } from './components/document/document.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    }
+    { path: '', component: HomeComponent},
+    { path: 'document', component: DocumentComponent}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
