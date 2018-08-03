6f38217f38be9d2fbd4379f8675c3a1178d4b280
Igor Meireles de Araújo <igoraraujo@ufpa.br>
2018-08-03 19:36:21 -0300

    Added ContentTools WYSIWYG editor from  https://github.com/GetmeUK/ContentTools

--- src/app/components/document/document.component.ts
+++ src/app/components/document/document.component.ts
@@ -1,3 +1,6 @@
+const ContentTools = require('assets/contentTools/content-tools.min');
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentComponent implements OnInit {
 
+  isShowDetails: boolean;
+  isEditable: boolean;
+  private editor: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.isShowDetails = false;
+    this.isEditable = false;
+    this.editor = ContentTools.EditorApp.get();
+    this.editor.init('*[data-editable]', 'main-content', null, false);
   }
+
+  showDetails(show: boolean) {
+    this.isShowDetails = show;
+  }
+
+  setEditable() {
+    this.isEditable = true;
+    // Starting the editor manually
+    this.editor.start();
+  }
+
+  saveDocument() {
+    this.isEditable = false;
+    // Stoping the editor manually (save)
+    this.editor.stop(true);
+  }
+
+  closeDocument() {
+    // Stoping the editor manually (cancel)
+    if (this.editor.stop(false)) {
+      this.isEditable = false;
+    }
+  }
+
+
 
 }
